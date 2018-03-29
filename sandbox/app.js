f7d3cb85c80237c8cade7cf94052412f0580b84b
Steven Huang <steven.charles.huang@gmail.com>
2018-03-29 16:02:07 -0400

    added User Stories. more sandboxing

--- sandbox/app.js
+++ sandbox/app.js
@@ -7,70 +7,70 @@ $(function() {
 // let makeDiv = $(document.body);
 // makeDiv.append('<div></div>')
 // console.log(makeDiv);
-  let blueBox = $('.blue-box');
-  let greenBox = $('.green-box');
-  let keys = {}
-
-$(document).keydown(function(e) {
-    keys[e.keyCode] = true;
-});
-
-$(document).keyup(function(e) {
-    delete keys[e.keyCode];
-});
+  // let blueBox = $('.blue-box');
+  // let greenBox = $('.green-box');
 // Motion Controls
-//https://stackoverflow.com/questions/7298507/move-element-with-keypress-multiple
-function moveSq() {
-    for (let direction in keys) {
-        if (!keys.hasOwnProperty(direction)) continue;
-        // 'a' moves left
-        else if (direction == 65) {
-            $(blueBox).animate({left: "-=5"}, 0);
-        }
-        // 'w' moves up
-        else if (direction == 87) {
-            $(blueBox).animate({top: "-=5"}, 0);
-        }
-        // 'd' moves right
-        else if (direction == 68) {
-            $(blueBox).animate({left: "+=5"}, 0);
-        }
-        // 's' moves down
-        else if (direction == 83) {
-            $(blueBox).animate({top: "+=5"}, 0);
-        }
-        // 'l' moves left
-        else if (direction == 76) {
-            $(greenBox).animate({left: "-=5"}, 0);
-        }
-        // 'p' moves up
-        else if (direction == 80) {
-            $(greenBox).animate({top: "-=5"}, 0);
-        }
-        // ''' moves right
-        else if (direction == 222) {
-            $(greenBox).animate({left: "+=5"}, 0);
-        }
-        // ';' moves down
-        else if (direction == 186) {
-            $(greenBox).animate({top: "+=5"}, 0);
-        }
-    }
-}
-moveSq();
-setInterval(moveSq, 10);
+// https://stackoverflow.com/questions/7298507/move-element-with-keypress-multiple
+//   let keys = {}
+
+// $(document).keydown(function(e) {
+//     keys[e.keyCode] = true;
+// });
+
+// $(document).keyup(function(e) {
+//     delete keys[e.keyCode];
+// });
+// function moveSq() {
+//     for (let direction in keys) {
+//         if (!keys.hasOwnProperty(direction)) continue;
+//         // 'a' moves left
+//         else if (direction == 65) {
+//             $(blueBox).animate({left: "-=5"}, 0);
+//         }
+//         // 'w' moves up
+//         else if (direction == 87) {
+//             $(blueBox).animate({top: "-=5"}, 0);
+//         }
+//         // 'd' moves right
+//         else if (direction == 68) {
+//             $(blueBox).animate({left: "+=5"}, 0);
+//         }
+//         // 's' moves down
+//         else if (direction == 83) {
+//             $(blueBox).animate({top: "+=5"}, 0);
+//         }
+//         // 'l' moves left
+//         else if (direction == 76) {
+//             $(greenBox).animate({left: "-=5"}, 0);
+//         }
+//         // 'p' moves up
+//         else if (direction == 80) {
+//             $(greenBox).animate({top: "-=5"}, 0);
+//         }
+//         // ''' moves right
+//         else if (direction == 222) {
+//             $(greenBox).animate({left: "+=5"}, 0);
+//         }
+//         // ';' moves down
+//         else if (direction == 186) {
+//             $(greenBox).animate({top: "+=5"}, 0);
+//         }
+//     }
+// }
+// moveSq();
+// setInterval(moveSq, 10);
 /////////////////////////////
 // MDN 2-D Hit Collision Example
 
-blueSq = {x: 5, y: 5, width: 50, height: 50}
-greenSq = {x: 20, y: 10, width: 10, height: 10}
+// blueSq = {x: 5, y: 5, width: 50, height: 50}
+// greenSq = {x: 20, y: 10, width: 10, height: 10}
 
-if (blueSq.x < greenSq.x + greenSq.width &&
-   blueSq.x + blueSq.width > greenSq.x &&
-   blueSq.y < greenSq.y + greenSq.height &&
-   blueSq.height + blueSq.y > greenSq.y) {
-    alert('collision detected!');
-}
+// if (blueSq.x < greenSq.x + greenSq.width &&
+//    blueSq.x + blueSq.width > greenSq.x &&
+//    blueSq.y < greenSq.y + greenSq.height &&
+//    blueSq.height + blueSq.y > greenSq.y) {
+//     alert('collision detected!');
+// }
 
 // filling in the values =>
 
@@ -127,6 +127,123 @@ if (blueSq.x < greenSq.x + greenSq.width &&
 //   $('.fight-screen').css('visibility', 'visible');
 //   $('.landing-screen').css('visibility', 'hidden');
 // });
+
+
+
+
+// Collision Detection Example
+// https://jsfiddle.net/jlr7245/217jrozd/3/
+//////////// COLLISION ALGO FROM MDN
+/*
+if (rect1.x < rect2.x + rect2.width &&
+   rect1.x + rect1.width > rect2.x &&
+   rect1.y < rect2.y + rect2.height &&
+   rect1.height + rect1.y > rect2.y) {
+
+}
+*/
+// declare collider obj
+const collider = {
+  // mDiv set to null
+  moveableDiv: null,
+  // staticDivs set to empty array
+  staticDivs: [],
+  // check collision function
+  checkCollision: function() {
+    // declare post collision var and initiate to false
+    let hasJustCollided = false;
+    // for loop - increment against length of static divs array
+    for (let i = 0; i < this.staticDivs.length; i++) {
+      // declare current Div, store collided Div's static divs
+      const currentDiv = this.staticDivs[i];
+      // MDN algorithm if / else arg
+      if (currentDiv.position.left < this.moveableDiv.position.left + this.moveableDiv.position.width &&
+      currentDiv.position.left + currentDiv.position.width > this.moveableDiv.position.left &&
+      currentDiv.position.top < this.moveableDiv.position.top + this.moveableDiv.position.height &&
+      currentDiv.position.height + currentDiv.position.top > this.moveableDiv.position.top) {
+        // result of MDN algorithm, just collided set to true
+        hasJustCollided = true;
+        // Collision true, if / else
+        if (!this.moveableDiv.ref.classList.contains('collision-state')) {
+          this.moveableDiv.ref.classList.add('collision-state');
+        }
+      } else if (this.moveableDiv.ref.classList.contains('collision-state') && !hasJustCollided) {
+          this.moveableDiv.ref.classList.remove('collision-state');
+        }
+    }
+  },
+
+};
+// BaseDiv class
+class BaseDiv {
+  constructor(position) {
+    this.position = position;
+  }
+}
+// MoveDiv class extends BaseDiv class
+class MoveDiv extends BaseDiv {
+  constructor(position, ref) {
+    super(position);
+    this.ref = ref;
+  }
+  // shift Position method
+  shiftPosition(x, y) {
+    this.position.left += x;
+    this.position.top += y;
+    this.reDraw();
+  }
+  // reDraw method
+  reDraw() {
+    this.ref.setAttribute('style', `left: ${this.position.left}px; top: ${this.position.top}px`);
+    collider.checkCollision();
+  }
+}
+// moveDiv function - key bindings
+function moveDiv(e) {
+  switch(e.which) {
+    case 37:
+      collider.moveableDiv.shiftPosition(-40, 0);
+      break;
+    case 38:
+      collider.moveableDiv.shiftPosition(0, -40);
+      break;
+    case 39:
+      collider.moveableDiv.shiftPosition(40, 0);
+      break;
+    case 40:
+      collider.moveableDiv.shiftPosition(0, 40);
+      break;
+    default:
+      console.log('not an arrow');
+      break;
+  }
+}
+// positionCreator functiomn
+function positionCreator(currentDiv) {
+  return {
+    left: currentDiv.getBoundingClientRect().left,
+    top: currentDiv.getBoundingClientRect().top,
+    height: currentDiv.getBoundingClientRect().height,
+    width: currentDiv.getBoundingClientRect().width
+  };
+}
+
+
+(() => {
+  const allTheDivs = document.querySelectorAll('.collideme');
+  for (let i = 0; i < allTheDivs.length; i++) {
+    const currentDiv = allTheDivs[i];
+    if (currentDiv.dataset.dynamic === 'true') {
+      currentDiv.setAttribute('style', 'left: 500px; top: 300px;');
+      const moveableDiv = new MoveDiv(positionCreator(currentDiv), currentDiv);
+      collider.moveableDiv = moveableDiv;
+    } else {
+      const staticDiv = new BaseDiv(positionCreator(currentDiv));
+      collider.staticDivs.push(staticDiv);
+    }
+  }
+  document.addEventListener('keydown', (e) => moveDiv(e));
+})();
 
 
 
---
+++ user-stories.md
@@ -0,0 +1,27 @@
+# User Stories
+
+
+## Landing Page
+
+As a user, when I arrive at the Landing Page, I want to see a logo displaying 
+the title of the game, enter my name, and start the game.
+
+## Game Initialization
+
+As a user, when the game initializes, I want to see avatars of my opponent's 
+character and my character, as well as our remaining Hit Points.
+
+## Playing The Game
+
+As a user, when I play the game, I want to move my character back and forth and
+attack my opponent.
+
+## Winning The Game
+
+As a user, I want to win the game when I have successfuly reduced my opponent's
+Hit Points to zero.
+
+## Game Reset
+
+As a user, when I have won the game, I want an option to replay the game or
+re-enter my name on the Landing Page.
